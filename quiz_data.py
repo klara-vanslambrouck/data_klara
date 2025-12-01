d9489d16563385ee6c40066169e5350d14de34fe
klara-vanslambrouck <zittovaklara@gmail.com>
2025-12-01 11:21:20 +0100

    moved quiz to main

---
+++ quiz_data.py
@@ -0,0 +1,56 @@
+quiz_data = [
+    {
+        "question": "1. Kdo z hlavních postav režíroval několik dílů Přátel?",
+        "choices": [
+            "a) Chandler",
+            "b) Ross",
+            "c) Phoebe"
+        ],
+        "answer": "b) Ross"
+    },
+    {
+        "question": "2. Která série měla nejvyšší sledovanost?",
+        "choices": [
+            "a) 2. série",
+            "b) 5. série",
+            "c) 8. série"
+        ],
+        "answer": "a) 2. série"
+    },
+    {
+        "question": "3. Která série neměla žádnou z epizod hodnocenou nad 9?",
+        "choices": [
+            "a) 7. série",
+            "b) 9. série",
+            "c) 10. série"
+        ],
+        "answer": "b) 9. série"
+    },
+    {
+        "question": "4. Kdo z Přátel byl nejpřekvapenější? (měl nejvyšší podíl replik s emocí „surprise“)",
+        "choices": [
+            "a) Monika",
+            "b) Ross",
+            "c) Rachel"
+        ],
+        "answer": "c) Rachel"
+    },
+    {
+        "question": "5. Která postava měla nejpozitivnější repliky? (nejvyšší podíl replik klasifikovaných jako „positive“)",
+        "choices": [
+            "a) Joey",
+            "b) Phoebe",
+            "c) Monika"
+        ],
+        "answer": "b) Phoebe"
+    },
+    {
+        "question": "6. Pro koho z Přátel je typické slovo „wedding“ (svatba)?",
+        "choices": [
+            "a) Rachel",
+            "b) Ross",
+            "c) Monika"
+        ],
+        "answer": "a) Monika"
+    }
+]
